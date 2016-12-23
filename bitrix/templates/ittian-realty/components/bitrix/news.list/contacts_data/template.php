e1638fcca2423e7b5eb3d987f1773247498c86f8
Roman Kulichkov <roman@kulichkov.pro>
2016-12-23 18:35:21 +0700

    Правка замена телефона скриптом калибки, включение композита, правка robots

---
+++ bitrix/templates/ittian-realty/components/bitrix/news.list/contacts_data/template.php
@@ -0,0 +1,25 @@
+<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();?>
+<?$this->setFrameMode(true);?>
+<?
+global $USER;
+if($USER->isAdmin())
+{
+    echo '<pre>';print_r($arResult['ITEMS']);echo '</pre>';
+}
+?>
+<?if(!count($arResult['ITEMS'])) return;?>
+
+<?foreach($arResult['ITEMS'] as $arItem){?>
+<?
+$this->AddEditAction($arItem["ID"], $arItem["EDIT_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+$this->AddDeleteAction($arItem["ID"], $arItem["DELETE_LINK"], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage("CT_BNL_ELEMENT_DELETE_CONFIRM")));
+?>
+
+<div class="top-b col-lg-4 <?=$arItem['PROPERTIES']['TYPE']['VALUE_XML_ID']?>" id="<?=$this->GetEditAreaId($arItem["ID"])?>">
+    <div class="b-wrap">
+        <div class="top"><?=$arItem['NAME']?></div>
+        <div class="bot <?=$arItem['PROPERTIES']['TYPE']['VALUE_XML_ID'] == 'hd-phone' ? 'phone' : ''?>"><?=nl2br($arItem['PROPERTIES']['TEXT']['VALUE'])?></div>
+    </div>
+</div>
+
+<?}?>
---
+++ include/inc_copyright.php
@@ -0,0 +1,2 @@
+<div class="col-lg-3" id="bx-composite-banner"></div>
+<div class="col-lg-3">© Застройщик ООО «СЛК», <?$x=2016; $now=getdate(); if($now['year']>$x){echo $now['year'];} else {echo $x;}?></div>
