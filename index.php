0aa6f86f0a9f5ff15b82d83772f3323074fcc032
Roman Kulichkov <roman@kulichkov.pro>
2017-02-22 15:09:46 +0700

    Поменял местами новости и акции

--- index.php
+++ index.php
@@ -83,6 +83,79 @@ $APPLICATION->IncludeComponent("bitrix:news.list",
     );?>
 <?}?>
 
+<?if($arTheme['SECTIONS']['NEWS_ONMAIN']['VALUE'] != 'hide'){?>
+    <div class="news">
+        <div class="wrapper">
+            <div class="big-title"><a href="<?=SITE_DIR?>news/"><?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_news_title.php", Array(), Array("MODE"=>"text"));?></a></div>
+
+            <?$APPLICATION->IncludeComponent(
+                "bitrix:news.list",
+                "news",
+                Array(
+                    "AJAX_MODE" => "N",
+                    "IBLOCK_TYPE" => "ittian_realty_content",
+                    "IBLOCK_ID" => "4",
+                    "NEWS_COUNT" => "5",
+                    "SORT_BY1" => "ACTIVE_FROM",
+                    "SORT_ORDER1" => "DESC",
+                    "SORT_BY2" => "SORT",
+                    "SORT_ORDER2" => "ASC",
+                    "FILTER_NAME" => "",
+                    "FIELD_CODE" => array(''),
+                    "PROPERTY_CODE" => array(""),
+                    "CHECK_DATES" => "Y",
+                    "DETAIL_URL" => "",
+                    "PREVIEW_TRUNCATE_LEN" => "",
+                    "ACTIVE_DATE_FORMAT" => "j F Y",
+                    "SET_TITLE" => "N",
+                    "SET_STATUS_404" => "N",
+                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                    "ADD_SECTIONS_CHAIN" => "Y",
+                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                    "PARENT_SECTION" => "",
+                    "PARENT_SECTION_CODE" => "",
+                    "INCLUDE_SUBSECTIONS" => "Y",
+                    "CACHE_TYPE" => "A",
+                    "CACHE_TIME" => "36000000",
+                    "CACHE_FILTER" => "N",
+                    "CACHE_GROUPS" => "Y",
+                    "PAGER_TEMPLATE" => "main",
+                    "DISPLAY_TOP_PAGER" => "N",
+                    "DISPLAY_BOTTOM_PAGER" => "Y",
+                    "PAGER_TITLE" => "",
+                    "PAGER_SHOW_ALWAYS" => "Y",
+                    "PAGER_DESC_NUMBERING" => "N",
+                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                    "PAGER_SHOW_ALL" => "Y",
+                    "AJAX_OPTION_JUMP" => "N",
+                    "AJAX_OPTION_STYLE" => "Y",
+                    "AJAX_OPTION_HISTORY" => "N",
+                    "HIDE_PAGER"=>"Y"
+                ),
+                false,
+                array('HIDE_ICONS' => 'Y')
+            );?>
+        </div>
+    </div>
+<?}?>
+
+
+    <div class="comp">
+        <div class="wrapper">
+            <div class="big-title"><a><?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_about_title.php", Array(), Array("MODE"=>"text"));?></a></div>
+
+            <div class="grid-row">
+                <div class="about col-lg-8">
+                    <?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_about.php", Array(), Array("MODE"=>"html"));?>
+                </div>
+                <div class="right col-lg-4">
+                    <div class="banner">
+                        <?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_banner.php", Array(), Array("MODE"=>"html"));?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?if($arTheme['SECTIONS']['SPEC_ONMAIN']['VALUE'] != 'hide'){?>
     <div class="news">
@@ -145,78 +218,5 @@ $APPLICATION->IncludeComponent("bitrix:news.list",
     </div>
 <?}?>
 
-    <div class="comp">
-        <div class="wrapper">
-            <div class="big-title"><a><?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_about_title.php", Array(), Array("MODE"=>"text"));?></a></div>
-
-            <div class="grid-row">
-                <div class="about col-lg-8">
-                    <?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_about.php", Array(), Array("MODE"=>"html"));?>
-                </div>
-                <div class="right col-lg-4">
-                    <div class="banner">
-                        <?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_banner.php", Array(), Array("MODE"=>"html"));?>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-
-<?if($arTheme['SECTIONS']['NEWS_ONMAIN']['VALUE'] != 'hide'){?>
-    <div class="news">
-        <div class="wrapper">
-            <div class="big-title"><a href="<?=SITE_DIR?>news/"><?$APPLICATION->IncludeFile(SITE_DIR."include/index/inc_news_title.php", Array(), Array("MODE"=>"text"));?></a></div>
-
-            <?$APPLICATION->IncludeComponent(
-                "bitrix:news.list",
-                "news",
-                Array(
-                    "AJAX_MODE" => "N",
-                    "IBLOCK_TYPE" => "ittian_realty_content",
-                    "IBLOCK_ID" => "4",
-                    "NEWS_COUNT" => "5",
-                    "SORT_BY1" => "ACTIVE_FROM",
-                    "SORT_ORDER1" => "DESC",
-                    "SORT_BY2" => "SORT",
-                    "SORT_ORDER2" => "ASC",
-                    "FILTER_NAME" => "",
-                    "FIELD_CODE" => array(''),
-                    "PROPERTY_CODE" => array(""),
-                    "CHECK_DATES" => "Y",
-                    "DETAIL_URL" => "",
-                    "PREVIEW_TRUNCATE_LEN" => "",
-                    "ACTIVE_DATE_FORMAT" => "j F Y",
-                    "SET_TITLE" => "N",
-                    "SET_STATUS_404" => "N",
-                    "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
-                    "ADD_SECTIONS_CHAIN" => "Y",
-                    "HIDE_LINK_WHEN_NO_DETAIL" => "N",
-                    "PARENT_SECTION" => "",
-                    "PARENT_SECTION_CODE" => "",
-                    "INCLUDE_SUBSECTIONS" => "Y",
-                    "CACHE_TYPE" => "A",
-                    "CACHE_TIME" => "36000000",
-                    "CACHE_FILTER" => "N",
-                    "CACHE_GROUPS" => "Y",
-                    "PAGER_TEMPLATE" => "main",
-                    "DISPLAY_TOP_PAGER" => "N",
-                    "DISPLAY_BOTTOM_PAGER" => "Y",
-                    "PAGER_TITLE" => "",
-                    "PAGER_SHOW_ALWAYS" => "Y",
-                    "PAGER_DESC_NUMBERING" => "N",
-                    "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
-                    "PAGER_SHOW_ALL" => "Y",
-                    "AJAX_OPTION_JUMP" => "N",
-                    "AJAX_OPTION_STYLE" => "Y",
-                    "AJAX_OPTION_HISTORY" => "N",
-                    "HIDE_PAGER"=>"Y"
-                ),
-                false,
-                array('HIDE_ICONS' => 'Y')
-            );?>
-        </div>
-    </div>
-<?}?>
 
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
